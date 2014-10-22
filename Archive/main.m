ee8ef56e22d0ff8c5bc974c75ed4c71610c9b272
Kendrick Daly <dalykendrick@Kendricks-MacBook-Pro.local>
2014-10-22 11:41:14 -0400

    Initial Commit

---
+++ Archive/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Archive
+//
+//  Created by Kendrick Daly on 10/22/14.
+//  Copyright (c) 2014 JACK Innovations L.L.C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
